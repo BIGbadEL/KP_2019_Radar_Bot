acf09ff4c96cf347812ac75af92ccad48b79469b
bigbadel <greg090912@gmail.com>
2019-11-26 13:38:23 +0100

    Added wifi support

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
 import serial
+import matplotlib.pyplot as plt
+import math
+import numpy
+import time
 
-ser = serial.Serial('/dev/ttyACM0',9600)
-readOut = 0
-while True:
-	readOut = int(ser.readline().decode('ascii'))
-	print (readOut)
+def polar_to_point( angle, radius):
+    return [radius * math.cos(angle), radius * math.sin(angle)]
+
+import matplotlib.pyplot as plt
+plt.ion()
+class DynamicUpdate():
+    min_x = -10
+    max_x = 10
+
+    def on_launch(self):
+        self.figure, self.ax = plt.subplots()
+        self.lines, = self.ax.plot([],[], 'o')
+        self.ax.set_autoscaley_on(True)
+        self.ax.grid()
+
+    def on_running(self, xdata, ydata):
+
+        self.lines.set_xdata(xdata)
+        self.lines.set_ydata(ydata)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+
+    def __call__(self):
+        self.on_launch()
+        xdata = []
+        ydata = []
+        ser = serial.Serial('/dev/ttyACM0',9600)
+        readOut = 0
+        while True:
+            readDistance = ser.readline().decode('ascii')[:-2]
+            floatDistance = float(readDistance)
+            readAngle = ser.readline().decode('ascii')[:-2]
+            floatAngle = float(readAngle)
+            point = polar_to_point(2.0 * math.pi * floatAngle / 360.0 , floatDistance)
+            xdata.append(point[0])
+            ydata.append(point[1])
+            self.on_running(xdata, ydata)
+        return xdata, ydata
+
+d = DynamicUpdate()
+d()
--- radar_arduino/radar_arduino.ino
+++ radar_arduino/radar_arduino.ino
@@ -1,6 +1,12 @@
 // Define Trig and Echo pin:
 #include <Stepper.h>
-  
+#include <SPI.h>
+#include <WiFi.h>
+#include <math.h>
+
+char ssid[] = "op6";     // the name of your network
+char pass[] = "blinku1234" // network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 #define trigPin 2
 #define echoPin 3
@@ -28,11 +34,35 @@ void setup() {
   //Begin Serial communication at a baudrate of 9600:
   Serial.begin(9600);
   t = millis();
+
+  Serial.println("Attempting to connect to WPA network...");
+  status = WiFi.begin(ssid, pass);
+
+   if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  
 }
+
+//double calculate_distance_form_points(double points[3]){
+//  double d01 = abs(points[0] - points[1]);
+//  double d02 = abs(points[0] - points[2]);
+//  double d12 = abs(points[1] - points[2]);
+//  if(d01 < d02) {
+//    if(d01 < d12) {
+//      
+//    }
+//  }
+//}
+
 void loop() {
   // Clear the trigPin by setting it LOW:
   int dt = millis() - t;
   t = millis();
+//  float distances[3];
+//  for(int i = 0; i < 3; i++){
+//    
+//  }
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   // Trigger the sensor by setting the trigPin high for 10 microseconds:
@@ -42,12 +72,14 @@ void loop() {
   // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
   duration = pulseIn(echoPin, HIGH);
   // Calculate the distance:
-  distance= duration*0.034/2;
+  distance = duration * 0.034 / 2;
   
   myStepper.step(stepsPerRevolution);
   double dalfa = 2.0 * PI / (60.0 * 1000.0) * dt;
   alfa += dalfa;
-  
+  if(alfa >= 360.0){
+    alfa -= 360.0;
+  }
   Serial.println(distance);
   Serial.println(360.0 * alfa / PI);
  }
